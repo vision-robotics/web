fbd29c10c141b898c793a9e0ea42b5da2a7184a2
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2023-06-18 16:22:09 +0300

    web ws files and the connection of ros2 web-bridge done

---
+++ web_ws/ros_web_ui/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Connection from "./Connection";
+
+class Home extends Component {
+  render() {
+    return (
+      <main>
+        <h1 className="text-center mt-3">Robot Control Page</h1>
+        <Connection />
+      </main>
+    );
+  }
+}
+
+export default Home;
