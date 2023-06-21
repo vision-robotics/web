6bcfcd3e2d2698287a853c2dfa7694cb7132c48f
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2023-06-21 21:58:45 +0300

    Succesfuly controling the robot from the web app

--- web_ws/ros_web_ui/src/components/Home.jsx
+++ web_ws/ros_web_ui/src/components/Home.jsx
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
 import Connection from "./Connection";
+import Teleopration from "./Teleopration";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Nav_Map from "./Nav_Map";
+//import { Row, Col, Container, Button } from "react-bootstrapt";
 
 class Home extends Component {
+  state = {};
   render() {
     return (
-      <main>
-        <h1 className="text-center mt-3">Robot Control Page</h1>
-        <Connection />
-      </main>
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center mt-3">Robot Control Page</h1>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Connection />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col></Col>
+            <Col>
+              <Nav_Map />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Teleopration />
+            </Col>
+            <Col>
+              <h1>Map</h1>
+              <p>This region will be used later for displaying the map</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
